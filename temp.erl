6083e8caec9566ab9ffc69bcb4d75691e120fb14
User Riccardo.ricci <riccardo.ricci@vm-081.vmx.nx>
2016-11-10 17:19:37 +0100

    Aggiunto il modulo temp.erl per la conversione delle temperature

---
+++ temp.erl
@@ -0,0 +1,13 @@
+-module(temp).
+-export([convert/2]).
+
+convert({fahrenheit, Temp}, celsius) ->
+  {celsius, 5 * (Temp - 32) / 9};
+convert({celsius, Temp}, fahrenheit) ->
+  {fahrenheit, 32 + Temp * 9 / 5};
+convert({reaumur, Temp}, celsius) ->
+  {celsius, 10 * Temp / 8};
+convert({celsius, Temp}, reaumur) ->
+  {reaumur, 8 * Temp / 10};
+convert({X, _}, Y) ->
+  {cannot,convert,X,to,Y}.
